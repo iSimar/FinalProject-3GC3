c89d262e2c98749f12e11230f8d1977bc78188b8
Simar Singh <singhs95@mcmaster.ca>
2014-12-14 17:55:15 -0500

    added windows.h tag if not apple

--- FinalProject/envBlock.cpp
+++ FinalProject/envBlock.cpp
@@ -13,6 +13,7 @@
 #  include <OpenGL/glu.h>
 #  include <GLUT/glut.h>
 #else
+#  include <windows.h>
 #  include <GL/gl.h>
 #  include <GL/glu.h>
 #  include <GL/freeglut.h>
--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -12,6 +12,7 @@
 #  include <OpenGL/glu.h>
 #  include <GLUT/glut.h>
 #else
+#  include <windows.h>
 #  include <GL/gl.h>
 #  include <GL/glu.h>
 #  include <GL/freeglut.h>
