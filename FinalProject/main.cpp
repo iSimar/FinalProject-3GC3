0931426af349aab6cd4af6771150a96388c3afae
Curran <currantam@hotmail.com>
2014-12-15 19:33:11 -0500

    added fog

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -36,6 +36,11 @@ float lightDir[] = {0, 0 ,1};
 float lightDif[] = {1, 1, 1, 1};
 float lightSpec[] = {1, 1, 1, 1 };
 
+GLuint filter;                      // Which Filter To Use
+GLuint fogMode[]= { GL_EXP, GL_EXP2, GL_LINEAR };   // Storage For Three Types Of Fog
+GLuint fogfilter= 2;                    // Which Fog To Use
+GLfloat fogColor[4]= {0.5f, 0.5f, 0.5f, 1.0f};      // Fog Color
+
 game * mainGame = new game();
 
 void display(){
@@ -62,6 +67,16 @@ void display(){
         mainGame->setFPLook();
     
     mainGame->draw();
+
+    glClearColor(0.5f,0.5f,0.5f,1.0f);          // We'll Clear To The Color Of The Fog ( Modified )
+ 
+    glFogi(GL_FOG_MODE, fogMode[fogfilter]);        // Fog Mode
+    glFogfv(GL_FOG_COLOR, fogColor);            // Set Fog Color
+    glFogf(GL_FOG_DENSITY, 0.35f);              // How Dense Will The Fog Be
+    glHint(GL_FOG_HINT, GL_DONT_CARE);          // Fog Hint Value
+    glFogf(GL_FOG_START, 45.0f);             // Fog Start Depth
+    glFogf(GL_FOG_END, 5.0f);               // Fog End Depth
+    glEnable(GL_FOG);                   // Enables GL_FOG
     
     glutSwapBuffers();
     
